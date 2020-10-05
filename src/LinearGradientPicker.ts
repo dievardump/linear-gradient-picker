0fb580fe3665ebf06f381c9265e7cb400ec56ee6
Simon Fremaux <dievardump@gmail.com>
2020-10-05 17:14:40 +0200

    format & lint

--- src/LinearGradientPicker.ts
+++ src/LinearGradientPicker.ts
@@ -9,8 +9,8 @@ export default class LinearGradientPicker implements LinearGradientPickerInterfa
     // find color before and after
     let selectedIndex: number = 0;
     for (let i = this.colors.length - 1; i > 0; i--) {
-			if (at >= this.colors[i].position) {
-				selectedIndex = i;
+      if (at >= this.colors[i].position) {
+        selectedIndex = i;
         break;
       }
     }
@@ -36,24 +36,24 @@ export default class LinearGradientPicker implements LinearGradientPickerInterfa
       a = left.color.a;
     }
 
-    return { r, g, b, a, };
+    return { r, g, b, a };
   }
 
-	/**
-	 * Add a color stop to the gradient
-	 */
+  /**
+   * Add a color stop to the gradient
+   */
   addColorStop(color: ColorMix, position: number): LinearGradientPicker {
-		// throw if position is not in [0; 100]
+    // throw if position is not in [0; 100]
     if ('number' !== typeof position || position < 0 || position > 100) {
       throw new Error('Position has to be a number in the range [0; 100]');
     }
 
-		// throw if a color stop is already at this position
+    // throw if a color stop is already at this position
     for (const item of this.colors) {
       if (item.position === position) {
         throw new Error(`Can't have two colors at the same position`);
       }
-		}
+    }
 
     // add color to the list
     const rgbaColor: RGBAColor = color_to_rgba(color);
@@ -70,18 +70,18 @@ export default class LinearGradientPicker implements LinearGradientPickerInterfa
     return this;
   }
 
-	/**
-	 * Retuns color at positions {at}
-	 *
-	 * @param at Number
-	 */
+  /**
+   * Retuns color at positions {at}
+   *
+   * @param at Number
+   */
   pick(at: number): RGBAColor {
-		// throw is no color were added
+    // throw is no color were added
     if (this.colors.length === 0) {
       throw new Error('You need to add at least 1 color.');
     }
 
-		// throw if at not in [0, 100]
+    // throw if at not in [0, 100]
     if ('number' !== typeof at || at < 0 || at > 100) {
       throw new Error('Picked position must be a number in the range [0; 100]');
     }
